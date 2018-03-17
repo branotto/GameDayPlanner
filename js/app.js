5f84e94f2d58b2649c6c4e9135e834dada2ba434
Brandon Otto <branaotto@gmail.com>
2018-03-17 10:49:57 -0400

    added js functionality to form submission

--- js/app.js
+++ js/app.js
@@ -9,8 +9,11 @@
 
 
 //Main handler for API Calls
-function handleAPIRequests(searchTerm)
+function handleAPIRequests(apiQuery)
 {
+
+    console.log(apiQuery);
+
     //Make Google Maps API Call
 
 
@@ -23,27 +26,53 @@ function handleAPIRequests(searchTerm)
 }
 
 
-//Listen for Form submission
-function handleSearch()
+//Listen for full form submission
+function handleFormSubmission(sportsQuery)
 {
-    
     $('#searchForm').on('submit', function(event){
-        let $searchQuery = $('#searchText').val();    
+        
         event.preventDefault();
         
-        handleAPIRequests($searchQuery);
+        let $location = $('#searchArea').val() ;
+
+        let $startDate = $('#startDate').val() ;
+
+        let $endDate = $('#endDate').val();
+
+        let apiQuery = {
+            sport : sportsQuery,
+            location : $location,
+            start : $startDate,
+            end : $endDate
+        };
+
+    handleAPIRequests(apiQuery);
+
+    });
+}
+
+
+//Listen for Sport Selection
+function handleSportSelection()
+{
+    
+    $('#js_sport').on('change', function(event){
+        let $sportSelection = $('#js_sport option:selected')
+        let sportQuery = $sportSelection.text();   
+
+        $("option[value='select']").attr("disabled", "disabled")
+
+        handleFormSubmission(sportQuery);
 
     });
 }
 
 
 //Document Ready Function
-function handleCallBack()
+function documentReady()
 {
-    
-    handleSearch();
+    handleSportSelection();  
 }
 
 
-
-$(handleCallBack)
+$(documentReady)
