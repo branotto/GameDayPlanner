6c5e200a04b4940c8c62055b6a40a020965315c6
Brandon Otto <branaotto@gmail.com>
2018-03-19 21:10:40 -0400

    reverted geocode to getJSON and added global variables for location and map

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 const API_KEY = 'AIzaSyC_LJ5t0klmo5LuUXUJ66U8Zv5eeQ2XevU';
 
+var coords = {
+    lat : 39.50,
+    lng : -98.35
+};
+
+var map;
+
 //Geocode the location
 function geocode(searchLocation){
     let query = {
@@ -8,47 +15,36 @@ function geocode(searchLocation){
         key : API_KEY 
     };
 
-    let coords;
-    $.ajax({
-        url: `${GEOCODE_URL}?address=${query.searchLocation}&key=${query.key}`,
-        async: false,
-        dataType: 'json',
-        success: function (response) {
-            coords = response.results[0].geometry.location;;
-          }
-    });
+    ($.getJSON(GEOCODE_URL, query, function(data){
+	    
+        coords = {
+            lat : data.results[0].geometry.location.lat,
+            lng : data.results[0].geometry.location.lng
+        }
 
-    return coords;
-    
+        let mapOptions  = {
+            zoom: 10,
+            center: coords
+        };
+        
+        initMap(mapOptions);
+    }));
 }
 
 //Handle Map Initiliaztion
 function initMap(mapOptions) {
-    console.log('mapoptions at init call' + mapOptions);
-    let map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
 
 //Main handler for API Calls
 function handleAPIRequests(apiQuery)
 {
+    //Find the location the user entered
+    geocode(apiQuery.location);
 
-    let mapOptions  = {
-        zoom: 4,
-        center: {
-            lat : 39.50,
-            lng : -98.35
-        }
-    };
-
-    console.log('mapoptions before' + mapOptions);
-    if(apiQuery.location !== "")
-    {
-        mapOptions.center = geocode(apiQuery.location);
-    }
-
-
-    //Make Google Maps API Call
-    initMap(mapOptions);
+    //Make Google Places API Call
+    //searchPlaces(coords)
 
     //Make EventBrite API Call
 
@@ -108,4 +104,4 @@ function documentReady()
 }
 
 
-$(documentReady)
+(documentReady());
